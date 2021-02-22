910753163ff29d4b84e86eb5d7f71f640fdfe6e0
toan0176 <xuantoan2948@gmail.com>
2021-02-22 09:49:18 +0100

    ...

--- index.php
+++ index.php
@@ -1,28 +1,30 @@
 <?php
 
-require_once 'config/init.php';
+// require_once 'config/init.php';
 
-$job = new Job;
-
-
-$template = new Template('templates/frontpage.php');
-
-$category = isset($_GET['category']) ? $_GET['category'] :null;
+// $job = new Job;
 
 
-$template->category = $category;
+// $template = new Template('templates/frontpage.php');
 
-if($category) {
-    $template->title = 'Search results';
-    $template->jobs = $job->getJobsByCategory($category);
-} else {
-    $template->title = 'Latest Job';
-    $template->jobs = $job->getAllJobs();
-}
+// $category = isset($_GET['category']) ? $_GET['category'] :null;
 
-$template->categories = $job->getCategories(); // transfer $categories to frontend.php
 
-echo $template;
+// $template->category = $category;
+
+// if($category) {
+//     $template->title = 'Search results';
+//     $template->jobs = $job->getJobsByCategory($category);
+// } else {
+//     $template->title = 'Latest Job';
+//     $template->jobs = $job->getAllJobs();
+// }
+
+// $template->categories = $job->getCategories(); // transfer $categories to frontend.php
+
+// echo $template;
+
+echo 'hello';
  
 ?>
 
